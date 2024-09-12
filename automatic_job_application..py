3910114f48cfcb65e5898e33ce23b75e9d61eccf
David Ogunmola <127635293+kdavid001@users.noreply.github.com>
2024-09-12 21:22:23 +0100

    Add files via upload

---
+++ automatic_job_application..py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+import time
+
+user_name = "korededavid03@gmail.com"
+password = "fyxry8-mawvIj-xawzok"
+
+var = ("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&"
+       "keywords=python%20developer&location=London%2C"
+       "%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
+
+url = 'https://www.linkedin.com/jobs/search'
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options)
+driver.get(var)
+
+sign_in = driver.find_element(By.XPATH, "/html/body/div[1]/header/nav/div/a[2]")
+sign_in.click()
+
+Enter_email = driver.find_element(By.ID, 'username')
+Enter_password = driver.find_element(By.XPATH, '//*[@id="password"]')
+
+Enter_email.send_keys(user_name)
+Enter_password.send_keys(password)
+time.sleep(2)
+Enter_email.send_keys(Keys.RETURN)
+# job_search.send_keys('python Developer')
+# location.send_keys('united kingdom')
+# job_search.send_keys(Keys.RETURN)
+
+
+# TODO: This needs an authentication most of the time you can still complete it later.
